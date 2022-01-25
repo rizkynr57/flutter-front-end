82a539914b0e69e84d5f3df1cb568c80c03193c5
rizkynr57 <rizky.nurahman48@gmail.com>
2022-01-25 23:46:54 +0500

    flutter

--- lib/sample.dart
+++ lib/sample.dart
@@ -8,11 +8,34 @@ class SampleWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.home),
-          title: Text("MyApplication"),
+          leading: const Icon(Icons.home),
+          title: const Text("MyApplication"),
           backgroundColor: Colors.deepPurple,
         ),
-        
+        body: Container(
+          padding: EdgeInsets.all(20),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(50),
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                spreadRadius: 3,
+                blurRadius: 2,
+              )
+            ],
+          ),
+          child: Text(
+            'MyApplications Page',
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.yellowAccent),
+          ),
+        ),
       ),
     );
   }
