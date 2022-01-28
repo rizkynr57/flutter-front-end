44eb0dbd480745cff74202b886d0dd977d54d6b7
rizkynr57 <rizky.nurahman48@gmail.com>
2022-01-29 04:36:10 +0500

    flutter

--- lib/latihan4.dart
+++ lib/latihan4.dart
@@ -68,6 +68,7 @@ class Latihan4 extends StatelessWidget {
                     padding: EdgeInsets.only(top: 10),
                   ),
                   Container(
+                    margin: EdgeInsets.all(10),
                     padding: EdgeInsets.all(10),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
@@ -80,13 +81,15 @@ class Latihan4 extends StatelessWidget {
                     ),
                     child: Container(
                       child: Text(
-                          '''             Pemandangan alam merupakan keindahan alam yang dapat dipersepsikan melalui komponen mata dalam pancaindra manusia.[1] Sketsa pemandangan alam dalam seni lukis biasanya terdiri dari komponen-komponen seperti pepohonan, semak-semak, daratan, bukit, lembah, air ataupun sejumlah benda buatan manusia contohnya rumah, jembatan, mobil dll.[2] Pemandangan alam merupakan suatu hal yang perlu dilindungi atau dijaga karena bertujuan untuk melindungi keindahan alam.[3] Indonesia merupakan salah satu negara yang cukup banyak memiliki pemandangan alam yang menarik dan indah misalnya negari Sianok (ngarai Sianok) di Sumatra Barat yang menjadi salah satu potensi wisata dengan fenomena alamnya yang Indah.
-                Indonesia banyak memiliki potensi keindahan alam yang memiliki daya tarik yang sangat beraneka ragam sehingga Indonesia merupakan salah satu negara yang paling diminati untuk tujuan pariwisata.[4] Selain pemandangan alam yang sangat potensial Indonesia juga merupakan negara yang kaya akan budaya sehingga dengan adanya variasi dan daya tarik tersendiri yang ditawarkan oleh Indonesia membuat Indonesia menjadi wilayah yang cukup menarik untuk dikunjungi.[4] Dalam kegiatan seni lukis pemandangan alam merupakan salah satu objek yang paling banyak diminati
+                          '''      Pemandangan alam merupakan keindahan alam yang dapat dipersepsikan melalui komponen mata dalam pancaindra manusia.[1] Sketsa pemandangan alam dalam seni lukis biasanya terdiri dari komponen-komponen seperti pepohonan, semak-semak, daratan, bukit, lembah, air ataupun sejumlah benda buatan manusia contohnya rumah, jembatan, mobil dll.[2] Pemandangan alam merupakan suatu hal yang perlu dilindungi atau dijaga karena bertujuan untuk melindungi keindahan alam.[3] Indonesia merupakan salah satu negara yang cukup banyak memiliki pemandangan alam yang menarik dan indah misalnya negari Sianok (ngarai Sianok) di Sumatra Barat yang menjadi salah satu potensi wisata dengan fenomena alamnya yang Indah.
+        Indonesia banyak memiliki potensi keindahan alam yang memiliki daya tarik yang sangat beraneka ragam sehingga Indonesia merupakan salah satu negara yang paling diminati untuk tujuan pariwisata.[4] Selain pemandangan alam yang sangat potensial Indonesia juga merupakan negara yang kaya akan budaya sehingga dengan adanya variasi dan daya tarik tersendiri yang ditawarkan oleh Indonesia membuat Indonesia menjadi wilayah yang cukup menarik untuk dikunjungi.[4] Dalam kegiatan seni lukis pemandangan alam merupakan salah satu objek yang paling banyak diminati
                           ''',
+                          textAlign: TextAlign.justify,
                           style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic)),
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          )),
                     ),
                   ),
                   Padding(
